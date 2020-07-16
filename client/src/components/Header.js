a5d8bde68e3829e22e64328464e2292682676124
Sophia Tan <sophia.tan.ad@gmail.com>
2020-07-15 17:04:21 -0700

    Added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -8,11 +9,15 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return (
-          <li><a href="/auth/google">Login With Google</a></li>
-        );
+        return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: ' 0 10px' }}>
+            Credits: { this.props.auth.credits }
+          </li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
