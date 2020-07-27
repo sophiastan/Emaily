68422cbd66552aafc7ee952a1fe8f44a50ba005d
Sophia Tan <sophia.tan.ad@gmail.com>
2020-07-27 15:29:44 -0700

    route to survey added, sendgrid implemented, starting off webhooks

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
+// All of our "action creators"
+// Action creators are functions that return actions!
+
+// export const changeLogin = shouldBeLoggedIn => {
+//   return {
+//     type: 'change_auth',
+//     payload: shouldBeLoggedIn
+//   };
+// };
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -11,4 +20,11 @@ export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({ type: FETCH_USER, paylod: res.data });
 };
